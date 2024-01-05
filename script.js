31ec48b8bff814fd97a404695457346c277a7fec
rober-web <robp.medeiros@gmail.com>
2024-01-05 22:51:52 +0000

    created getPassordOptions first options to validate user input regarding the number of characters

--- script.js
+++ script.js
@@ -1,133 +1,144 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
 
 // Function to prompt user for password options
 const getPasswordOptions = () => {
-  //Length of password
-  let passLength = prompt('Enter the number of characters you want to generate for your password. It must be between 8 and 128 characters.');
-  //validate length of password
-  for(i = 0; i < passLength.length; i++) {
-    console.log(passLength[i]);
+  // Length of password
+  let passLength;
+
+  // Validate length of password
+  //create a while loop to ensure the correct length is returned
+  while (true) {
+    passLength = parseInt(
+      prompt(
+        "Enter the number of characters you want to generate for your password. It must be between 8 and 128 characters."
+      )
+    );
+    /*check whether the user choice attends the characters criteria of the app*/
+    if (passLength < 8 || passLength > 128) {
+      alert(
+        "Please, ensure your password length is between 8 and 128 characters."
+      );
+    } else if (isNaN(passLength) /*check if it is not a number*/) {
+      alert(
+        "You inserted not a valid number, please make sure you insert numbers between 8 and 128."
+      );
+    } else {
+      alert(passLength + " is a good choice!");
+      break;
+    }
   }
-/*   if(passLength.length < 8 || passLength.length > 128){
-    alert('Please, ensure your password length is between 8 and 128 characters.')
-  } */
 
+  // This alert will only be executed after the while loop breaks
+  alert("Proceed to the next step");
+};
 
-}
+// Call the function to prompt the user for password options
+getPasswordOptions();
 
 // Function for getting a random element from an array
-const getRandom = (arr) => {
-
-}
+const getRandom = (arr) => {};
 
 // Function to generate password with user input
-const generatePassword = () => {
-
-}
+const generatePassword = () => {};
 
 // Get references to the #generate element
-const generateBtn = document.querySelector('#generate');
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 const writePassword = () => {
   let password = generatePassword();
-  let passwordText = document.querySelector('#password');
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
+};
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
-
-
-//start the code
-
-getPasswordOptions();
+generateBtn.addEventListener("click", writePassword);
